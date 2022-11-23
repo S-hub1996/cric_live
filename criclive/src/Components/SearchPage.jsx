aa2b972bf040d04733a9368b81fb410eadbec3fb
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-11-23 17:50:43 +0530

    some

--- criclive/src/Components/SearchPage.jsx
+++ criclive/src/Components/SearchPage.jsx
@@ -1,9 +1,15 @@
+import { Image,Input,Button,Flex } from '@chakra-ui/react'
 import React from 'react'
 
 const SearchPage = () => {
   return (
     <div>
-        
+<Flex m='4' gap={4}>
+
+      <Input placeholder='Search teams, Players etc'/>
+      <Button w='40%' _hover={'none'} color='white'  bg='#984337'>Search</Button>
+</Flex>
+        <Image src='https://img.freepik.com/premium-vector/cricket-championship-concept_1302-17559.jpg?auto=format&h=200' w='100%'/>
 
     </div>
   )
