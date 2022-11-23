d94c385875c563a9e9452158604f0f876dd32850
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-11-23 15:39:58 +0530

    .

--- criclive/src/Components/Hero.jsx
+++ criclive/src/Components/Hero.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const Hero = () => {
   return (
           <Box>
-        
+         
        <AspectRatio maxW='560px' ratio={1}>
-  <iframe
+  <iframe 
      title='naruto'
     src='https://www.youtube.com/embed/QhBnZ6NPOY0'
     allowFullScreen
