0279615c63b8ec2fdbccee4a898481e0938c0716
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-11-23 14:24:55 +0530

    video

---
+++ criclive/src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Hero = () => {
+  return (
+          <Box>
+        
+       <AspectRatio maxW='560px' ratio={1}>
+  <iframe
+     title='naruto'
+    src='https://www.youtube.com/embed/QhBnZ6NPOY0'
+    allowFullScreen
+  />
+</AspectRatio>
+          </Box>
+  )
+}
+
+export default Hero
