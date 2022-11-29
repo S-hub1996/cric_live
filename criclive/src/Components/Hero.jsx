7004010cbcaae8828dc01e702e71b4d878dd0769
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-11-29 20:11:32 +0530

    edit

--- criclive/src/Components/Hero.jsx
+++ criclive/src/Components/Hero.jsx
@@ -1,24 +1,16 @@
 import { Box } from "@chakra-ui/react";
-import { Heading } from "@chakra-ui/react";
 import React from "react";
-import video from '../Assets/heroVideo.mp4'
-import './hero.css'
+import video from "../Assets/heroVideo.mp4";
+import "./hero.css";
 const Hero = () => {
   // const video=
   return (
-    // <Box >
-<Box>
-        {/* <div className="overlay"></div> */}
-        <div className="outer">
+    <Box>
+      <div className="outer">
         <video src={video} autoPlay loop muted />
-              {/* <Heading className="heading">wlcm to criclive</Heading> */}
-              </div>
+      </div>
     </Box>
-    // </Box>
   );
 };
 
 export default Hero;
-     
-  
-
