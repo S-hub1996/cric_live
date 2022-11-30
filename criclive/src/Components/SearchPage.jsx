6c0b08b62df368fa598c4c58e937676d944d2916
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-11-30 13:22:26 +0530

    carsouls in search

--- criclive/src/Components/SearchPage.jsx
+++ criclive/src/Components/SearchPage.jsx
@@ -1,30 +1,32 @@
-import { Image, Input, Button, Flex, SimpleGrid, Heading } from "@chakra-ui/react";
+import { Image, Input, Button, Flex, SimpleGrid, Heading, Box } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { searchMatch } from "./api";
 import Cards from "./Card";
 import SearchCard from "./SearchCard";
+import './slide.css'
+import Carousel from "./Slider";
 const SearchPage = () => {
   const [input, setinput] = useState("");
   const [data, setdata] = useState("");
 
   function handleSearch() {
-    console.log(input) 
+    console.log(input)
     searchMatch(input)
-    .then((data) => {
-      console.log(data);
-      setdata(data.data);
-    })
-    
-    .catch((error) => {});
-  }
-  console.log(data) 
+      .then((data) => {
+        console.log(data);
+        setdata(data.data);
+      })
 
-  useEffect(()=>{
-if(data.length){
-  setdata(data)
-}
-    
-  },[data])
+      .catch((error) => { });
+  }
+  console.log(data)
+
+  useEffect(() => {
+    if (data.length) {
+      setdata(data)
+    }
+
+  }, [data])
   return (
     <div>
       <Flex m="4" gap={4}>
@@ -45,24 +47,28 @@ if(data.length){
           Search
         </Button>
       </Flex>
-<Heading textAlign={'center'}>Search Results</Heading>
-      <SimpleGrid columns={[1, 2, 3,3,5]}>
+      <Heading textAlign={'center'}>Search Results</Heading>
+      <SimpleGrid columns={[1, 2, 3, 3, 5]}>
 
-        {data?data?.map((match) => {
-            // if (match.teamInfo)
-            return <SearchCard match={match} key={match.id}/>
-        }):null}
+        {data ? data?.map((match) => {
+          // if (match.teamInfo)
+          return <SearchCard match={match} key={match.id} />
+        }) : null}
 
-    </SimpleGrid>
+      </SimpleGrid>
 
 
-      <Image
+      {/* <Image
         src="https://img.freepik.com/premium-vector/cricket-championship-concept_1302-17559.jpg?auto=format&h=200"
         w="100%"
-      />
+      /> */}
+<Box w={'full'}>
 
+    
+  <Carousel/>
+</Box>
 
-    </div>
+    </div > 
   );
 };
 
